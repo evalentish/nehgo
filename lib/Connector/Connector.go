19ad28e37465f6de748fb515526432d0b1c9bbf3
Thomas Björk <thomas.bjork@evalent.com>
2018-04-01 18:34:41 +0200

    Update

--- lib/Connector/Connector.go
+++ lib/Connector/Connector.go
@@ -1,5 +1,13 @@
 package Connector
 
+import (
+	"net/http"
+	"strings"
+	"log"
+	"io/ioutil"
+	"fmt"
+)
+
 type Connector struct {
 	username string
 	password string
@@ -8,11 +16,56 @@ type Connector struct {
 }
 
 func (connector *Connector) Get(address string, payload string) string {
+	var url string
+	url = "https://" + connector.hostname + "/__API__" + address
+	client := &http.Client{}
+	request, err := http.NewRequest("PUT", url, strings.NewReader(payload))
+	request.SetBasicAuth(connector.username, connector.password)
+	request.ContentLength = int64(len(payload))
+	response, err := client.Do(request)
+	if err != nil {
+		log.Fatal(err)
+	} else {
+		defer response.Body.Close()
+		contents, err := ioutil.ReadAll(response.Body)
+		if err != nil {
+			log.Fatal(err)
+		}
+		fmt.Println("The calculated length is:", len(string(contents)), "for the url:", url)
+		fmt.Println("   ", response.StatusCode)
+		hdr := response.Header
+		for key, value := range hdr {
+			fmt.Println("   ", key, ":", value)
+		}
+		fmt.Println(contents)
+	}
 	return ""
 }
 
 func (connector *Connector) Put(address string, payload string) string {
-
+	var url string
+	url = "https://" + connector.hostname + address
+	client := &http.Client{}
+	request, err := http.NewRequest("PUT", url, strings.NewReader(payload))
+	request.SetBasicAuth(connector.username, connector.password)
+	request.ContentLength = int64(len(payload))
+	response, err := client.Do(request)
+	if err != nil {
+		log.Fatal(err)
+	} else {
+		defer response.Body.Close()
+		contents, err := ioutil.ReadAll(response.Body)
+		if err != nil {
+			log.Fatal(err)
+		}
+		fmt.Println("The calculated length is:", len(string(contents)), "for the url:", url)
+		fmt.Println("   ", response.StatusCode)
+		hdr := response.Header
+		for key, value := range hdr {
+			fmt.Println("   ", key, ":", value)
+		}
+		fmt.Println(contents)
+	}
 	return ""
 }
 
