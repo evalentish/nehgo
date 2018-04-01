efef91e933a6ee821f1b7813e4cd2118dedb8451
Thomas Björk <thomas.bjork@evalent.com>
2018-04-01 18:46:07 +0200

    Update

--- lib/Connector/Connector.go
+++ lib/Connector/Connector.go
@@ -21,7 +21,8 @@ func (connector *Connector) Get(address string, payload string) string {
 	client := &http.Client{}
 	request, err := http.NewRequest("PUT", url, strings.NewReader(payload))
 	request.SetBasicAuth(connector.username, connector.password)
-	//request.ContentLength = int64(len(payload))
+	request.ContentLength = int64(len(payload))
+	fmt.Printf("Length %d", request.ContentLength)
 	response, err := client.Do(request)
 	if err != nil {
 		log.Fatal(err)
