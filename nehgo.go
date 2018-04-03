aca0e905c89b77e755785b286357eebfb1e397bb
Thomas Björk <thomas.bjork@evalent.com>
2018-04-03 06:36:50 +0200

    Update

--- nehgo.go
+++ nehgo.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"github.com/evalentish/nehgo/lib/Order"
 	"github.com/evalentish/nehgo/lib/Connector"
+	"nehgo/lib/Customer"
 )
 
 func GetOrder(connector *Connector.Connector, order_id int) *Order.Order {
@@ -17,4 +18,17 @@ func GetOrder(connector *Connector.Connector, order_id int) *Order.Order {
 	}
 	order = Order.NewOrder(apiResult.Content)
 	return order
+}
+
+func GetCustomer(connector *Connector.Connector, customer_id int) *Customer.Customer {
+	var apiResult *Connector.ConnectorResponse
+	var customer *Customer.Customer
+	var address string
+	address = fmt.Sprintf("/customer/%d", customer_id)
+	apiResult = connector.Get(address)
+	if apiResult == nil {
+		return nil
+	}
+	customer = Customer.NewCustomer(apiResult.Content)
+	return customer
 }
